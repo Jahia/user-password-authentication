66681a143ea8192ae8e123dfd50809537c7224b7
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-09-18 18:08:00 +0200

    test: add additional cases

--- tests/cypress/e2e/graphQL.errors.cy.ts
+++ tests/cypress/e2e/graphQL.errors.cy.ts
@@ -2,27 +2,58 @@ import 'cypress-mailpit';
 import {createUserForMFA, initiate, installMFAConfig, prepare} from './utils';
 import {deleteUser} from '@jahia/cypress';
 
-const userName = 'test_mfa_user';
-const password = 'password';
+const usr = 'test_mfa_user';
+const pwd = 'password';
 const email = 'testmfauser@example.com';
 
 describe('Error scenarios common to all factors', () => {
     before(() => {
-        createUserForMFA(userName, password, email);
+        createUserForMFA(usr, pwd, email);
         installMFAConfig('fake.yml');
     });
+
     after(() => {
-        deleteUser(userName);
+        deleteUser(usr);
         installMFAConfig('disabled.yml');
     });
 
-    it('Should throw an error when invalid credential is provided', () => {
-        initiate('unknownUser', 'myPassword', 'Invalid username or password');
+    // Invalid cases for the test user to be checked
+    const INVALID_CASES = [
+        {
+            description: 'invalid username',
+            username: 'unknownUser',
+            password: pwd,
+            expectedError: 'Invalid username or password'
+        },
+        {
+            description: 'invalid password',
+            username: usr,
+            password: 'myPassword',
+            expectedError: 'Invalid username or password'
+        },
+        {
+            description: 'empty username',
+            username: '',
+            password: pwd,
+            expectedError: 'Invalid username or password'
+        },
+        {
+            description: 'empty password',
+            username: usr,
+            password: '',
+            expectedError: 'Invalid username or password'
+        }
+    ];
+
+    INVALID_CASES.forEach(({description, username, password, expectedError}) => {
+        it(`Should throw an error when ${description} is provided`, () => {
+            initiate(username, password, expectedError);
+        });
     });
 
     it('Should throw an error when an unregistered factor gets prepared', () => {
         cy.log('1- initiate');
-        initiate(userName, password);
+        initiate(usr, pwd);
 
         cy.log('2- prepare');
         prepare('unknown_factor', 'Factor type not supported: unknown_factor');
