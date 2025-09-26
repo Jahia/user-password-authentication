30dae852d36bae76dda6deec28e7b5068a6a49cb
Baptiste Grimaud <bgrimaud@jahia.com>
2025-09-26 11:18:26 +0200

    cleanup import

--- api/src/main/java/org/jahia/modules/mfa/impl/MfaServiceImpl.java
+++ api/src/main/java/org/jahia/modules/mfa/impl/MfaServiceImpl.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
-import javax.jcr.Value;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
