49d08d0eb802d8e2a22ebe0a53abb8087956c7d2
Baptiste Grimaud <bgrimaud@jahia.com>
2025-11-21 13:39:59 +0100

    javadoc

--- api/src/main/java/org/jahia/modules/mfa/gql/GqlFactorsMutation.java
+++ api/src/main/java/org/jahia/modules/mfa/gql/GqlFactorsMutation.java
@@ -3,7 +3,14 @@ package org.jahia.modules.mfa.gql;
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLName;
 
-// TODO is this name OK?
+/**
+ * GraphQL extension point for MFA factor-specific mutations.
+ * <p>
+ * This class serves as a base type that can be extended by custom MFA factor implementations
+ * to add their own GraphQL mutation endpoints.
+ *
+ * @see org.jahia.modules.mfa.emailcode.gql.GqlEmailCodeFactorMutationExtension for an example
+ */
 @GraphQLName("MfaFactorsMutation")
 @GraphQLDescription("Access MFA factors specific mutations")
 public class GqlFactorsMutation {
