7ea63ec1f1b4984423fff2dea2bee627b15dcc3d
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-04 17:24:25 +0100

    WIP

--- tests/ci.postrun.sh
+++ tests/ci.postrun.sh
@@ -2,3 +2,14 @@
 source ./set-env.sh
 
 docker logs smtp-server > ./artifacts/results/smtp-server.log
+
+echo " == Collecting JacCoCo execution data =="
+
+docker cp jahia:/jacoco-data/jacoco-cypress.exec ./artifacts/results/jacoco-cypress.exec
+
+echo " == Generating JaCoCo Cypress report =="
+
+(cd .. && mvn compile org.jacoco:jacoco-maven-plugin:report -Djacoco.dataFile=tests/artifacts/results/jacoco-cypress.exec && mv target/site/jacoco target/site/jacoco-cypress  )
+
+# TODO
+ls -al -R
