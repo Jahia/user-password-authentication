8873332c5c250831a505d7eee64d0aa466582626
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-04 18:04:23 +0100

    save

--- tests/ci.postrun.sh
+++ tests/ci.postrun.sh
@@ -9,7 +9,19 @@ docker cp jahia:/jacoco-data/jacoco-cypress.exec ./artifacts/results/jacoco-cypr
 
 echo " == Generating JaCoCo Cypress report =="
 
-(cd .. && mvn compile org.jacoco:jacoco-maven-plugin:report -Djacoco.dataFile=tests/artifacts/results/jacoco-cypress.exec && mv target/site/jacoco target/site/jacoco-cypress  )
+(cd .. && mvn compile org.jacoco:jacoco-maven-plugin:report -Djacoco.dataFile=tests/artifacts/results/jacoco-cypress.exec && mv target/site/jacoco target/site/jacoco-cypress)
+
+echo " == Creating JaCoCo report archive =="
+
+# Create zip archive with classifier
+(cd ../target/site && zip -r jacoco-cypress-report.zip jacoco-cypress)
+
+echo " == Uploading to Nexus =="
+
+# Retrieve Maven properties
+GROUP_ID=$(cd .. && mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout)
+ARTIFACT_ID=$(cd .. && mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+VERSION=$(cd .. && mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 # TODO
 ls -al -R
