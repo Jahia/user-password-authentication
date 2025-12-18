9e955f845306841450e7ca02333b9a1a1bdeef2f
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-18 15:09:08 +0100

    rename mfa-ui -> ui

--- tests/cypress/e2e/utils/content.ts
+++ tests/cypress/e2e/utils/content.ts
@@ -30,7 +30,7 @@ export function createSiteWithLoginPage(siteKey: string, languages = [I18N.defau
         serverName: serverName,
         templateSet: 'jahia-user-password-authentication-template-set-test-module'
     });
-    enableModule('jahia-user-password-authentication-mfa-ui', siteKey);
+    enableModule('jahia-user-password-authentication-ui', siteKey);
     const titleProps = languages.map(language => ({name: 'jcr:title', value: `Login page (${language})`, language: language}));
     const properties = [...titleProps, {name: 'j:templateName', value: 'mfa-authentication-page'}];
     addNode({
