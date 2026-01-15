70e42c6f97cf4700ea118e507829a4e48d8b05c3
Baptiste Grimaud <bgrimaud@jahia.com>
2026-01-15 18:11:42 +0100

    remove unused wrap element for dangerouslySetInnerHTML

--- ui/src/templates/EmailCode/emailCodeTemplate.server.tsx
+++ ui/src/templates/EmailCode/emailCodeTemplate.server.tsx
@@ -21,11 +21,10 @@ jahiaComponent(
     renderContext.getMainResource().getDependencies().add(renderContext.getSite().getPath());
 
     return (
-      <div
+      <html
+        lang="en"
         dangerouslySetInnerHTML={{
-          __html: `<!DOCTYPE html>
-<html lang="en">
-<head>
+          __html: `<head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="x-apple-disable-message-reformatting" />
