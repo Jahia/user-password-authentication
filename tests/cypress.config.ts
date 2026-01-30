eb8a2834e8960da8aad32d3f3250da731bebda19
Francois G. <fgerthoffert@jahia.com>
2026-01-30 09:15:12 +0100

    fixed linting errors

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -1,5 +1,5 @@
 import {defineConfig} from 'cypress';
-import fs from 'fs'
+import fs from 'fs';
 
 export default defineConfig({
     // DefaultCommandTimeout: 10000,
@@ -24,16 +24,16 @@ export default defineConfig({
                 (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
                     if (results && results.video) {
                         // Do we have failures for any retry attempts?
-                        const failures = results.tests.some((test) =>
-                        test.attempts.some((attempt) => attempt.state === 'failed')
-                        )
+                        const failures = results.tests.some(test =>
+                            test.attempts.some(attempt => attempt.state === 'failed')
+                        );
                         if (!failures) {
-                        // delete the video if the spec passed and no tests retried
-                        fs.unlinkSync(results.video)
+                            // Delete the video if the spec passed and no tests retried
+                            fs.unlinkSync(results.video);
                         }
                     }
                 }
-            )            
+            );
             // eslint-disable-next-line @typescript-eslint/no-var-requires
             return require('./cypress/plugins/index.js')(on, config);
         },
