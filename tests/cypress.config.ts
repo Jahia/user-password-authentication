3912baec7dfba7b98edc53635c000474396512e4
Francois G. <fgerthoffert@jahia.com>
2026-01-30 08:37:38 +0100

    test: Added video recording for failing tests

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'cypress';
+import fs from 'fs'
 
 export default defineConfig({
     // DefaultCommandTimeout: 10000,
@@ -8,6 +9,7 @@ export default defineConfig({
         configFile: 'reporter-config.json'
     },
     screenshotsFolder: './results/screenshots',
+    video: true, // In Cypress, videos are disabled by default
     videosFolder: './results/videos',
     viewportWidth: 1366,
     viewportHeight: 768,
@@ -16,6 +18,22 @@ export default defineConfig({
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
+            // Delete videos for tests that did not fail
+            on(
+                'after:spec',
+                (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+                    if (results && results.video) {
+                        // Do we have failures for any retry attempts?
+                        const failures = results.tests.some((test) =>
+                        test.attempts.some((attempt) => attempt.state === 'failed')
+                        )
+                        if (!failures) {
+                        // delete the video if the spec passed and no tests retried
+                        fs.unlinkSync(results.video)
+                        }
+                    }
+                }
+            )            
             // eslint-disable-next-line @typescript-eslint/no-var-requires
             return require('./cypress/plugins/index.js')(on, config);
         },
